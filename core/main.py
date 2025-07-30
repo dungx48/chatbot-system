f68612987be793640c10bb11411c5ea4adc58f2e
vdungx <vdungx@vdungxs-MacBook-Pro.local>
2025-07-30 18:45:10 +0700

    [dungpv] build docker compose

--- core/main.py
+++ core/main.py
@@ -2,7 +2,9 @@ from decouple import config
 import sys
 import uvicorn
 
+from common.config import settings
+
 if __name__ == "__main__":
     sys.path.insert(0, '..')
-    port = int(config('PORT'))
+    port=settings.PORT
     uvicorn.run("core.app:app", host="0.0.0.0", port=port, reload=True)
