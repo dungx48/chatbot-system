228436eb7a274534c4c1deb23b31513dd5ec4696
dungx48 <dung.mstk56@gmail.com>
2025-08-10 12:58:48 +0700

    [dungpv] clean file

--- run_docker_compose.bat
+++
@@ -1,3 +0,0 @@
-docker-compose -f cicd/docker-compose.yml down
-docker-compose -f cicd/docker-compose.yml up --build
-docker-compose restart mychatbot
--- run_dockerfile.bat
+++
@@ -1,2 +0,0 @@
-docker build -f cicd/Dockerfile -t mychatbot .
-docker run -d --name mychatbot_container -p 8000:8000 mychatbot
